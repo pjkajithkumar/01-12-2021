7ea13a367daa12443fc1ca9fe81d481b93ed3ff0
AJITH KUMAR P J K <47271245+pjkajithkumar@users.noreply.github.com>
2021-12-02 17:00:52 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # print a nice greeting.
-def say_hello(username = "Ajith Kumar's Real World 2021-new2-branch"):
+def say_hello(username = "Ajith Kumar's Real World 2021-new2-branch-YES"):
     return '<p>Hello %s!</p>\n' % username
 
 # some bits of text for the page.
